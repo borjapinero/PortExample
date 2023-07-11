ddc1c34ca1887e3ff9363728a235f071e58b0adf
borjapinero <128033467+borjapinero@users.noreply.github.com>
2023-07-11 11:18:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -107,6 +107,8 @@ resource "azurerm_virtual_machine" "example" {
   resource_group_name   = var.resource_group_name
   network_interface_ids = [azurerm_network_interface.example.id]
   vm_size               = var.vm_size
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
 
   storage_image_reference {
     publisher = "Canonical"
